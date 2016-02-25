09b66d6bcda5b24cdf3ec29bc2285674efe7486d
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2016-02-25 17:06:32 +0100

    removed accidentally attached build files

--- build/lib/fscrape/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-from fscrape.TwitterPublisher import TwitterPublisher
-from fscrape.TwitterSubscriber import TwitterSubscriber
-
-sub = TwitterSubscriber('standard', 'Trump')
-pub = TwitterPublisher('standard', 'Trump')
--- build/lib/fscrape/main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-def main():
-	
-	
-if __name__ == 'main':
-	main()
